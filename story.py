944ba67e8b11dbbae446f6550795a716f4723ad7
Biswajeet Srichandan <155133588+Biswajeet124@users.noreply.github.com>
2024-09-28 12:40:46 +0530

    Biswajeet
    Updated the comments

--- story.py
+++ story.py
@@ -1,16 +1,18 @@
-import streamlit as st
-import google.generativeai as genai
-from gtts import gTTS
-import base64
-from io import BytesIO
-import re
-from concurrent.futures import ThreadPoolExecutor
-#import os
+import streamlit as st   #Used for creating and deploying the web application interface.
+import google.generativeai as genai   #Provides access to Google's Gemini AI model.
+from gtts import gTTS  #Google Text-to-Speech library for audio generation.
+import base64   #Used for encoding audio data for web playback.
+
+# BytesIO stores it in the computer's memory (RAM). This can be much faster for certain operations, especially when dealing with smaller amounts of data.
+from io import BytesIO   #Allows working with bytes in memory.
+
+import re   #Regular expressions module for word counting.
+from concurrent.futures import ThreadPoolExecutor   #Enables concurrent execution for performance improvement. Speeding up I/O-bound tasks. Parallelizing CPU-bound tasks
 
 # Web page configuration
 st.set_page_config(page_title="Interactive AI Storyteller", page_icon="📚", layout="wide")
 
-# Custom CSS
+# Applies custom CSS for styling the application. The unsafe_allow_html=True allows the use of HTML in the markdown.
 st.markdown("""
 <style>
     .stApp {
@@ -34,6 +36,6 @@ st.markdown("""
         margin-bottom: 20px;
     }
 </style>
-""", unsafe_allow_html=True)
+""", unsafe_allow_html=True)   
 # Configuration for Google Gemini API
 genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
