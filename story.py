7cae3f072402b96f2ec2a8bd1814abc672a03d93
Dibya004 <dibyajyotiacharya326@gmail.com>
2024-09-28 11:42:46 +0530

    Committed by Dibyajyoti

--- story.py
+++ story.py
@@ -5,3 +5,7 @@ import base64
 from io import BytesIO
 import re
 from concurrent.futures import ThreadPoolExecutor
+#import os
+
+# Set page config
+st.set_page_config(page_title="Interactive AI Storyteller", page_icon="📚", layout="wide")
