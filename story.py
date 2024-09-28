9cbefba5c4923d91344e31ad45655022148e46db
Biswajeet Srichandan <155133588+Biswajeet124@users.noreply.github.com>
2024-09-28 15:05:58 +0530

    Update story.py

--- story.py
+++ story.py
@@ -1,19 +1,13 @@
-import streamlit as st   #Used for creating and deploying the web application interface.
-import google.generativeai as genai   #Provides access to Google's Gemini AI model.
-from gtts import gTTS  #Google Text-to-Speech library for audio generation.
-import base64   #Used for encoding audio data for web playback.
+import streamlit as stl  
+import google.generativeai as gai   
+from gtts import gTTS 
+import base64 
+from io import BytesIO  
+import re  
+from concurrent.futures import ThreadPoolExecutor
 
-# BytesIO stores it in the computer's memory (RAM). This can be much faster for certain operations, especially when dealing with smaller amounts of data.
-from io import BytesIO   #Allows working with bytes in memory.
-
-import re   #Regular expressions module for word counting.
-from concurrent.futures import ThreadPoolExecutor   #Enables concurrent execution for performance improvement. Speeding up I/O-bound tasks. Parallelizing CPU-bound tasks
-
-# Web page configuration
-st.set_page_config(page_title="Interactive AI Storyteller", page_icon="📚", layout="wide")
-
-# Applies custom CSS for styling the application. The unsafe_allow_html=True allows the use of HTML in the markdown.
-st.markdown("""
+stl.set_page_config(page_title="Interactive AI Storyteller", page_icon="📚", layout="wide")
+stl.markdown("""
 <style>
     .stApp {
         background-color: var(--background-color);
@@ -38,24 +32,16 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)   
 
-st.logo("https://play-lh.googleusercontent.com/QIez0eyZVAi_EvKssJmsn8nlgC024DWjmUrRmTMNvUb06-BIbQuYi1HJwuHfdF-w")
-st.sidebar.markdown("MetaNarrate AI")
-
-# Configuration for Google Gemini API
-genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
-
-# Initialize the generative model
-@st.cache_resource
+stl.logo("https://play-lh.googleusercontent.com/QIez0eyZVAi_EvKssJmsn8nlgC024DWjmUrRmTMNvUb06-BIbQuYi1HJwuHfdF-w")
+stl.sidebar.markdown("MetaNarrate AI")
+gai.configure(api_key=stl.secrets["GOOGLE_API_KEY"])
+@stl.cache_resource
 def get_model():
-    return genai.GenerativeModel('gemini-1.5-flash')
-
+    return gai.GenerativeModel('gemini-1.5-flash')
 model = get_model()
-
-# Function to generate content with improved error handling
 def generate_content(prompt, safety_settings):
     try:
         response = model.generate_content(prompt, safety_settings=safety_settings)
-        
         if response.parts:
             return response.text
         elif hasattr(response, 'prompt_feedback'):
@@ -67,6 +53,5 @@ def generate_content(prompt, safety_settings):
         else:
             return "Unable to generate content. Please try again with a different input."
     except Exception as e:
-        st.error(f"An error occurred: {str(e)}")
+        stl.error(f"An error occurred: {str(e)}")
         return "An error occurred. Please try again."
-
